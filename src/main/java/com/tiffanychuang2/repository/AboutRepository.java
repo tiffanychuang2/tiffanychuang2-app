d93230586fc5c2a9e4948b92e29e63912eca5783
Tiffany <tiffany.chuang2@gmail.com>
2019-03-05 23:35:40 -0500

    navigate to oneAbout

--- src/main/java/com/tiffanychuang2/repository/AboutRepository.java
+++ src/main/java/com/tiffanychuang2/repository/AboutRepository.java
@@ -1,9 +1,15 @@
 package com.tiffanychuang2.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
+import org.yaml.snakeyaml.tokens.Token.ID;
 
 import com.tiffanychuang2.model.OneAbout;
 
 public interface AboutRepository extends CrudRepository<OneAbout, Long> {
 
+	Optional<OneAbout> findById(ID id);
+//	Optional<OneAbout> findById(Long id);
+	
 }
